8518eb0e41a6c7f0da9bb24b7702613e14070043
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-10-22 22:08:03 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,6 +1,6 @@
 # Provided, don't edit
 require 'directors_database'
-
+require 'pry'
 # A method we're giving you. This "flattens"  Arrays of Arrays so: [[1,2],
 # [3,4,5], [6]] => [1,2,3,4,5,6].
 
@@ -34,35 +34,31 @@ end
 # Your code after this point
 
 def movies_with_director_key(name, movies_collection)
-  # GOAL: For each Hash in an Array (movies_collection), provide a collection
-  # of movies and a directors name to the movie_with_director_name method
-  # and accumulate the returned Array of movies into a new Array that's
-  # returned by this method.
-  #
-  # INPUT:
-  # * name: A director's name
-  # * movies_collection: An Array of Hashes where each Hash represents a movie
-  #
-  # RETURN:
-  #
-  # Array of Hashes where each Hash represents a movie; however, they should all have a
-  # :director_name key. This addition can be done by using the provided
-  # movie_with_director_name method
+  movie_with_director = []
+  index = 0
+  while index < movies_collection.length do
+    combine = movie_with_director_name(name, movies_collection[index])
+    movie_with_director << combine
+    index += 1
+  end
+  movie_with_director
 end
 
 
 def gross_per_studio(collection)
-  # GOAL: Given an Array of Hashes where each Hash represents a movie,
-  # return a Hash that includes the total worldwide_gross of all the movies from
-  # each studio.
-  #
-  # INPUT:
-  # * collection: Array of Hashes where each Hash where each Hash represents a movie
-  #
-  # RETURN:
-  #
-  # Hash whose keys are the studio names and whose values are the sum
-  # total of all the worldwide_gross numbers for every movie in the input Hash
+  gps = {}
+  i = 0
+  while i < collection.length do
+    ww_gross = collection[i][:worldwide_gross]
+    studio = collection[i][:studio]
+    if !gps[studio]
+      gps[studio] = 0
+    else
+      gps[studio] += ww_gross
+    end
+    i += 1
+  end
+  gps
 end
 
 def movies_with_directors_set(source)
